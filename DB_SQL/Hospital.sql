de5f5fc41333a01877f5ce2446fdf88f0295a03f
Omar1998Hassa <39214394+Omar1998Hassa@users.noreply.github.com>
2020-05-08 14:27:33 +0200

    sql Identity

--- DB_SQL/Hospital.sql
+++ DB_SQL/Hospital.sql
@@ -6,7 +6,7 @@ use Hospital
 ------------Table Creation-----------------
 Create Table Patient
 (
-PID bigint NOT NULL,
+PID bigint identity NOT NULL,
 Name VARCHAR(30) NOT NULL, 
 Username VARCHAR(15) NOT NULL,
 Password VARCHAR(15) NOT NULL,
@@ -32,7 +32,7 @@ Create Table Admin
 
 Create Table Receptionist
 (
-RecID bigint NOT NULL,
+RecID bigint identity NOT NULL,
 Name VARCHAR(30) NOT NULL, 
 Username VARCHAR(15) NOT NULL,
 Password VARCHAR(15) NOT NULL,
@@ -48,7 +48,7 @@ UNIQUE (Username)
 
 Create Table Department
 (
- DepID int NOT NULL CHECK (DepID >= 1 AND DepID <= 100),
+ DepID int identity NOT NULL CHECK (DepID >= 1 AND DepID <= 100),
  Name VARCHAR(30) NOT NULL,
  PRIMARY KEY (DepID),
  UNIQUE (Name)
@@ -56,7 +56,7 @@ Create Table Department
 
 Create Table Doctor
 (
-DID bigint NOT NULL,
+DID bigint identity NOT NULL,
 Name VARCHAR(30) NOT NULL,
 Username VARCHAR(15) NOT NULL, 
 Password VARCHAR(15) NOT NULL,
@@ -77,7 +77,7 @@ FOREIGN KEY (Department_ID) REFERENCES Department
 
 Create Table Scan
 (
- SID int NOT NULL CHECK (SID >= 1 AND SID <= 100),
+ SID int identity NOT NULL CHECK (SID >= 1 AND SID <= 100),
  Name VARCHAR(30),
  Price float NOT NULL CHECK (Price >= 100 AND Price <= 1000),
  PRIMARY KEY (SID)
@@ -85,7 +85,7 @@ Create Table Scan
 
 Create Table Medication
 (
- MID int NOT NULL CHECK (MID >= 1 AND MID <= 500),
+ MID int identity NOT NULL CHECK (MID >= 1 AND MID <= 500),
  Name VARCHAR(30),
  Price float NOT NULL CHECK (Price >= 20 AND Price <= 1000),
  Exp_Date DATETIME CHECK (Exp_Date > GETDATE()), 
@@ -95,7 +95,7 @@ Create Table Medication
 
 Create Table Room
 (
- RID int NOT NULL CHECK (RID >= 1 AND RID <= 500),
+ RID int identity NOT NULL CHECK (RID >= 1 AND RID <= 500),
  Type VARCHAR(16) CHECK (Type = 'Examination Room' OR Type = 'Surgery Room' OR Type = 'ICU'),
  Size FLOAT  CHECK (Size >= 30 AND Size <= 60),
  Price FLOAT NOT NULL CHECK (Price >= 100 AND Price <= 1000),
@@ -107,7 +107,7 @@ Create Table Room
 
 Create Table Registration 
 (
-RegID bigint NOT NULL,
+RegID bigint identity NOT NULL,
 Reciptionist_ID bigint NOT NULL, 
 Doctor_ID bigint NOT NULL,
 Patient_ID bigint NOT NULL,
@@ -157,12 +157,13 @@ FOREIGN KEY (Patient_ID) REFERENCES Patient,
 FOREIGN KEY (Doctor_ID) REFERENCES Doctor
 );
 
-
-insert into Patient 
+SET IDENTITY_INSERT Patient ON
+insert into Patient(PID,Name,Username,Password,Gender,Age,Mobile_Number,Address)
 values
 (1,'Mohamad', 'M16','1234', 'M',5,'01145585123','doky'),
 (2,'Ibrahim', 'I20','1234', 'M',20,'01145585193','October'),
 (3,'Fatma', 'F90','1234', 'F',7,'01245585193','Giza')
+SET IDENTITY_INSERT Patient OFF
 
 SET IDENTITY_INSERT Admin ON
 insert into Admin (AID,Name,Username,Password)
@@ -172,49 +173,61 @@ Values
 (30, 'Samir','S61','1234')
 SET IDENTITY_INSERT Admin OFF
 
-
-insert into Receptionist
+SET IDENTITY_INSERT Receptionist ON
+insert into Receptionist(RecID,Name,Username,Password,Gender,Age,Mobile_Number,Address,Salary)
 Values
 (26, 'Ahmed','A50','1234','M',26,'01247585193','Cairo',5000),
 (30, 'hamad','H90','1234','M',45,'01347585193','Mansora',7000),
 (40, 'Remas','HM15','1234','F',27,'01247595193','Fayoom',9000)
+SET IDENTITY_INSERT Receptionist OFF
 
-insert into Department
+SET IDENTITY_INSERT Department ON
+insert into Department(DepID,Name)
 Values
 (1,'Brain'),
 (2, 'heart'),
 (3, 'Eyes')
+SET IDENTITY_INSERT Department OFF
 
-insert into Doctor
+SET IDENTITY_INSERT Doctor ON
+insert into Doctor(DID,Name,Username,Password,Gender,Age,Mobile_Number,Address,Department_ID,Start_of_Working_Hours,End_of_Working_Hours,Salary,Avg_Rating)
 values
 (1,'Diaa','D76','1234','M',25,'01247599193','Dubi',1,'08 AM' ,'06 PM' ,10000,10),
 (2,'Nour','N78','1234','F',29,'01747599193','Germany',2,'08 AM','06 PM',30000,10),
 (3,'Nesreen','N59','1234','F',31,'01247599190','Italy',3,'08 AM','06 PM',80000,10)
+SET IDENTITY_INSERT Doctor Off
 
-insert into Scan
+SET IDENTITY_INSERT Scan ON
+insert into Scan(SID,Name,Price)
 Values
 (1,'MRI', 200),
 (2,'Blood',300),
 (3, 'XRay',400)
+SET IDENTITY_INSERT Scan OFF
 
-insert into Medication
+SET IDENTITY_INSERT Medication ON
+insert into Medication(MID,Name,Price,Exp_Date)
 Values
 (1,'koha',100,'2030-05-10'),
 (2, 'Soda3',50,'2050-09-16'),
 (3, 'vitamins',90,'2090-04-19')
+SET IDENTITY_INSERT Medication OFF
 
-insert into Room 
+SET IDENTITY_INSERT Room  ON
+insert into Room(RID,Type,Size,Price,Department_ID,Room_Availability) 
 Values
 (1,'Examination Room',40,900,1,0),
 (2,'Examination Room',50,800,2,1),
 (3,'Examination Room',35,950,3,0)
+SET IDENTITY_INSERT Room  OFF
 
-insert into Registration
+SET IDENTITY_INSERT Registration ON
+insert into Registration(RegID,Reciptionist_ID,Doctor_ID,Patient_ID,Room_ID,Date,Reserved_Time_Slot,State,Diagnosis,Comments)
 Values
 (1,26, 1, 1, 1, GETDATE(), '08 AM', 'Waiting', NULL, NULL),
 (2,30, 2, 2, 2, GETDATE(), '09 AM', 'Waiting', NULL, NULL),
 (3,40, 3, 3, 3, GETDATE(), '10 AM', 'Waiting', NULL, NULL)
-
+SET IDENTITY_INSERT Registration OFF
 
 insert into Ordered_Scans
 Values
