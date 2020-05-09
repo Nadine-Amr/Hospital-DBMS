aac38dce4be0a7791691a6a593103921a4fb957f
Omar1998Hassa <39214394+Omar1998Hassa@users.noreply.github.com>
2020-05-09 19:16:48 +0200

    my View and statistics stuff

---
+++ DBapplication/ViewsAndStatisticsForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace DBapplication
+{
+    partial class ViewsAndStatisticsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ViewDoctorButton = new System.Windows.Forms.Button();
+            this.ViewDepartmentsButton = new System.Windows.Forms.Button();
+            this.ViewReceptionistsButton = new System.Windows.Forms.Button();
+            this.ViewPatientsButton = new System.Windows.Forms.Button();
+            this.ViewScansButton = new System.Windows.Forms.Button();
+            this.LogOutbutton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.ViewDoctor_RatingsButton = new System.Windows.Forms.Button();
+            this.ViewOrdered_ScansButton = new System.Windows.Forms.Button();
+            this.ViewRegistrationsButton = new System.Windows.Forms.Button();
+            this.ViewRoomsButton = new System.Windows.Forms.Button();
+            this.ViewMedicationsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ViewDoctorButton
+            // 
+            this.ViewDoctorButton.Location = new System.Drawing.Point(12, 12);
+            this.ViewDoctorButton.Name = "ViewDoctorButton";
+            this.ViewDoctorButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewDoctorButton.TabIndex = 0;
+            this.ViewDoctorButton.Text = "ViewDoctors";
+            this.ViewDoctorButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewDepartmentsButton
+            // 
+            this.ViewDepartmentsButton.Location = new System.Drawing.Point(12, 99);
+            this.ViewDepartmentsButton.Name = "ViewDepartmentsButton";
+            this.ViewDepartmentsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewDepartmentsButton.TabIndex = 1;
+            this.ViewDepartmentsButton.Text = "ViewDepartments";
+            this.ViewDepartmentsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewReceptionistsButton
+            // 
+            this.ViewReceptionistsButton.Location = new System.Drawing.Point(12, 70);
+            this.ViewReceptionistsButton.Name = "ViewReceptionistsButton";
+            this.ViewReceptionistsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewReceptionistsButton.TabIndex = 2;
+            this.ViewReceptionistsButton.Text = "ViewReceptionists";
+            this.ViewReceptionistsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewPatientsButton
+            // 
+            this.ViewPatientsButton.Location = new System.Drawing.Point(12, 41);
+            this.ViewPatientsButton.Name = "ViewPatientsButton";
+            this.ViewPatientsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewPatientsButton.TabIndex = 3;
+            this.ViewPatientsButton.Text = "ViewPatients";
+            this.ViewPatientsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewScansButton
+            // 
+            this.ViewScansButton.Location = new System.Drawing.Point(12, 128);
+            this.ViewScansButton.Name = "ViewScansButton";
+            this.ViewScansButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewScansButton.TabIndex = 4;
+            this.ViewScansButton.Text = "ViewScans";
+            this.ViewScansButton.UseVisualStyleBackColor = true;
+            // 
+            // LogOutbutton
+            // 
+            this.LogOutbutton.Location = new System.Drawing.Point(337, 403);
+            this.LogOutbutton.Name = "LogOutbutton";
+            this.LogOutbutton.Size = new System.Drawing.Size(114, 23);
+            this.LogOutbutton.TabIndex = 5;
+            this.LogOutbutton.Text = "Log Out";
+            this.LogOutbutton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(12, 403);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(114, 23);
+            this.BackButton.TabIndex = 6;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ViewDepartments";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 330);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "ViewDepartments";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 301);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(114, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "ViewDepartments";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // ViewDoctor_RatingsButton
+            // 
+            this.ViewDoctor_RatingsButton.Location = new System.Drawing.Point(12, 272);
+            this.ViewDoctor_RatingsButton.Name = "ViewDoctor_RatingsButton";
+            this.ViewDoctor_RatingsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewDoctor_RatingsButton.TabIndex = 10;
+            this.ViewDoctor_RatingsButton.Text = "ViewDoctor_Ratings";
+            this.ViewDoctor_RatingsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewOrdered_ScansButton
+            // 
+            this.ViewOrdered_ScansButton.Location = new System.Drawing.Point(12, 243);
+            this.ViewOrdered_ScansButton.Name = "ViewOrdered_ScansButton";
+            this.ViewOrdered_ScansButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewOrdered_ScansButton.TabIndex = 11;
+            this.ViewOrdered_ScansButton.Text = "ViewOrdered_Scans";
+            this.ViewOrdered_ScansButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewRegistrationsButton
+            // 
+            this.ViewRegistrationsButton.Location = new System.Drawing.Point(12, 214);
+            this.ViewRegistrationsButton.Name = "ViewRegistrationsButton";
+            this.ViewRegistrationsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewRegistrationsButton.TabIndex = 12;
+            this.ViewRegistrationsButton.Text = "ViewRegistrations";
+            this.ViewRegistrationsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewRoomsButton
+            // 
+            this.ViewRoomsButton.Location = new System.Drawing.Point(12, 186);
+            this.ViewRoomsButton.Name = "ViewRoomsButton";
+            this.ViewRoomsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewRoomsButton.TabIndex = 13;
+            this.ViewRoomsButton.Text = "ViewRooms";
+            this.ViewRoomsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewMedicationsButton
+            // 
+            this.ViewMedicationsButton.Location = new System.Drawing.Point(12, 157);
+            this.ViewMedicationsButton.Name = "ViewMedicationsButton";
+            this.ViewMedicationsButton.Size = new System.Drawing.Size(114, 23);
+            this.ViewMedicationsButton.TabIndex = 14;
+            this.ViewMedicationsButton.Text = "ViewMedications";
+            this.ViewMedicationsButton.UseVisualStyleBackColor = true;
+            // 
+            // ViewsAndStatisticsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 450);
+            this.Controls.Add(this.ViewMedicationsButton);
+            this.Controls.Add(this.ViewRoomsButton);
+            this.Controls.Add(this.ViewRegistrationsButton);
+            this.Controls.Add(this.ViewOrdered_ScansButton);
+            this.Controls.Add(this.ViewDoctor_RatingsButton);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.LogOutbutton);
+            this.Controls.Add(this.ViewScansButton);
+            this.Controls.Add(this.ViewPatientsButton);
+            this.Controls.Add(this.ViewReceptionistsButton);
+            this.Controls.Add(this.ViewDepartmentsButton);
+            this.Controls.Add(this.ViewDoctorButton);
+            this.Name = "ViewsAndStatisticsForm";
+            this.Text = "ViewsAndStatisticsForm";
+            this.Load += new System.EventHandler(this.ViewsAndStatisticsForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ViewDoctorButton;
+        private System.Windows.Forms.Button ViewDepartmentsButton;
+        private System.Windows.Forms.Button ViewReceptionistsButton;
+        private System.Windows.Forms.Button ViewPatientsButton;
+        private System.Windows.Forms.Button ViewScansButton;
+        private System.Windows.Forms.Button LogOutbutton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button ViewDoctor_RatingsButton;
+        private System.Windows.Forms.Button ViewOrdered_ScansButton;
+        private System.Windows.Forms.Button ViewRegistrationsButton;
+        private System.Windows.Forms.Button ViewRoomsButton;
+        private System.Windows.Forms.Button ViewMedicationsButton;
+    }
+}
---
+++ DBapplication/ViewsAndStatisticsForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public partial class ViewsAndStatisticsForm : Form
+    {
+        public ViewsAndStatisticsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewsAndStatisticsForm_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+    }
+}
