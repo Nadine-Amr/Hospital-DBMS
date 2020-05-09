78527fbfd8f33d2d756f8b563f5db5a367b69ecf
MohammedEl-sayedAhmed <mohammedelsayed4648@gmail.com>
2020-05-09 06:15:21 +0200

    Adding queries in controller

--- DBapplication/Controller.cs
+++ DBapplication/Controller.cs
@@ -131,42 +131,374 @@ namespace DBapplication
             return dbMan.ExecuteReader(query);
         }
 
-        public int DAssignMedications(long DoctorID,String PatientName, String MedicationName, decimal Dosage=0)
+        public int DAssignMedications(long DoctorID, String PatientName, String MedicationName, decimal Dosage = 0)
         {
             string query;
             if (Dosage == 0)
             {
-                 query = "Insert into Prescribed_Medications(Medication_ID, Registration_ID, Prescription_Time) "
-                           + " select RegID, MID, GETDATE() "
-                           + "From Registration as R, Medication as M"
-                           + "where M.MID = (select MID from Medication where Name = ' " + MedicationName + " ') AND R.RegID = (select Top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '" + PatientName + "')) AND(Doctor_ID =" + DoctorID + ") Order By Date Desc )";
+                query = "Insert into Prescribed_Medications(Medication_ID, Registration_ID, Prescription_Time) "
+                          + " select RegID, MID, GETDATE() "
+                          + "From Registration as R, Medication as M"
+                          + "where M.MID = (select MID from Medication where Name = ' " + MedicationName + " ') AND R.RegID = (select Top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '" + PatientName + "')) AND(Doctor_ID =" + DoctorID + ") Order By Date Desc )";
             }
             else
             {
-                 query = "Insert into Prescribed_Medications(Medication_ID, Registration_ID, Prescription_Time) "
-                           + " select RegID, MID, GETDATE(), "+Dosage+" "
-                           + "From Registration as R, Medication as M"
-                           + "where M.MID = (select MID from Medication where Name = ' " + MedicationName + " ') AND R.RegID = (select Top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '" + PatientName + "')) AND(Doctor_ID =" + DoctorID + ") Order By Date Desc )";
+                query = "Insert into Prescribed_Medications(Medication_ID, Registration_ID, Prescription_Time) "
+                          + " select RegID, MID, GETDATE(), " + Dosage + " "
+                          + "From Registration as R, Medication as M"
+                          + "where M.MID = (select MID from Medication where Name = ' " + MedicationName + " ') AND R.RegID = (select Top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '" + PatientName + "')) AND(Doctor_ID =" + DoctorID + ") Order By Date Desc )";
             }
-           
-            return dbMan.ExecuteNonQuery(query);        
+
+            return dbMan.ExecuteNonQuery(query);
         }
 
         public int DRequestScans(long DoctorID, String PatientName, String ScanName)
         {
-           string query = "Insert into Ordered_Scans(Scan_ID, Registration_ID, Scan_Start_Time, Scan_End_Time) "
-           + " select RegID, SID,GETDATE(),dateadd(hh,+1,R.Reserved_Time_Slot)"
-           + "From Registration as R, Scan as S"
-           + "where S.SID = (select SID from Scan where Name ='"+ScanName+"') AND R.RegID =(select Top 1 RegID from Registration where (Patient_ID = (select PID from Patient where Name='"+PatientName+"')) AND (Doctor_ID ="+DoctorID+") Order By Date Desc )";
+            string query = "Insert into Ordered_Scans(Scan_ID, Registration_ID, Scan_Start_Time, Scan_End_Time) "
+            + " select RegID, SID,GETDATE(),dateadd(hh,+1,R.Reserved_Time_Slot)"
+            + "From Registration as R, Scan as S"
+            + "where S.SID = (select SID from Scan where Name ='" + ScanName + "') AND R.RegID =(select Top 1 RegID from Registration where (Patient_ID = (select PID from Patient where Name='" + PatientName + "')) AND (Doctor_ID =" + DoctorID + ") Order By Date Desc )";
             return dbMan.ExecuteNonQuery(query);
         }
 
-        public String GetScanEndTime(long DoctorID, String PatientName,String ScanName)
+        public String GetScanEndTime(long DoctorID, String PatientName, String ScanName)
         {
 
             string query = "Select Scan_End_Time from Ordered_Scans as O, Registration as R, Scan as S"
-                        +"where O.Registration_ID = R.RegID And O.Scan_ID = S.SID AND S.SID = (select SID from Scan where Name = '"+ScanName+"') AND R.RegID = (select top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '"+PatientName+"')) AND(Doctor_ID = "+DoctorID+") Order By Date Desc )";
+                        + "where O.Registration_ID = R.RegID And O.Scan_ID = S.SID AND S.SID = (select SID from Scan where Name = '" + ScanName + "') AND R.RegID = (select top 1 RegID from Registration where(Patient_ID = (select PID from Patient where Name= '" + PatientName + "')) AND(Doctor_ID = " + DoctorID + ") Order By Date Desc )";
             return dbMan.ExecuteNonQuery(query).ToString();
         }
+
+
+        //A_AddUpdateAdmin
+        public int A_AddAdmin(string name, string username, string password)
+        {
+            string query = "INSERT INTO Admin (Name, Username, Password) VALUES (" +
+               "'" + name + "'," +
+               "'" + username + "'," +
+               "'" + password + "')";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+        public int A_UpdateAdmin(string name, string username, string password, int selectedAdmin)
+        {
+            string query = "update Admin set " +
+               "Name='" + name + "'," +
+               "Username='" + username + "'," +
+               "Password='" + password + "'" +
+               "where AID = " + selectedAdmin + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public DataTable SelectAdmin_name_id()
+        {
+            string query = "SELECT Name, AID FROM Admin;";
+            return dbMan.ExecuteReader(query);
+        }
+
+        public DataTable SelectAdmin(string name)
+        {
+            string query = "SELECT Name, Username, Password FROM Admin where Name=' " + name + "'";
+            return dbMan.ExecuteReader(query);
+        }
+
+        //A_AddDoctor
+        public int A_AddDoctor(string name, string username, string password, string gender, int age, int mobile, string address, int deptID, string startWH, string endWH, int salary, double avgRate)
+        {
+            string query = "INSERT INTO Doctor (Name, Username, Password, Gender, Age, Mobile_Number, Address, Department_ID, Start_of_Working_Hours,End_of_Working_Hours,Salary, Avg_Rating) VALUES (" +
+               "'" + name + "'," +
+               "'" + username + "'," +
+               "'" + password + "'," +
+               "'" + gender + "'," +
+               "" + age + "," +
+               "" + mobile + "," +
+               "'" + address + "'," +
+               "" + deptID + "," +
+               "'" + startWH + "'," +
+               "'" + endWH + "'," +
+               "" + salary + "," +
+               "" + avgRate + ")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int A_UpdateDoctor(string name, string username, string password, string gender, int age, int mobile, string address, int deptID, string startWH, string endWH, int salary, double avgRate, int selectedDrID)
+        {
+            string query = "update Doctor set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'," +
+               "Department_ID=" + deptID + "," +
+               "Start_of_Working_Hours='" + startWH + "'," +
+               "End_of_Working_Hours='" + endWH + "'," +
+               "Salary=" + salary + "," +
+               "Avg_Rating=" + avgRate +
+               "where DID = " + selectedDrID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*------------------------A_AddUpdateReceptionist-----------------------*/
+
+        public int A_AddRecep(string name, string username, string password, string gender, int age, int mobile, string address, int salary)
+        {
+            string query = "INSERT INTO Receptionist (Name, Username, Password, Gender, Age, Mobile_Number, Address, Salary) VALUES (" +
+               "'" + name + "'," +
+               "'" + username + "'," +
+               "'" + password + "'," +
+               "'" + gender + "'," +
+               "" + age + "," +
+               "" + mobile + "," +
+               "'" + address + "'," +
+               "" + salary +")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+        
+        public int A_UpdateRecep(string name, string username, string password, string gender, int age, int mobile, string address,int salary, int selectedRecepID)
+        {
+            string query = "update Receptionist set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'," +
+               "Salary=" + salary +
+               "where RecID = " + selectedRecepID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*----------------------------R_AddUpdatePatient------------------------*/
+      
+        public int R_AddPatient(string name, string username, string password, string gender, int age, int mobile, string address)
+        {
+            string query = "INSERT INTO Patient (Name, Username, Password, Gender, Age, Mobile_Number, Address) VALUES (" +
+               "'" + name + "'," +
+               "'" + username + "'," +
+               "'" + password + "'," +
+               "'" + gender + "'," +
+               "" + age + "," +
+               "" + mobile + "," +
+               "'" + address + "'" + ")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int R_UpdatePatient(string name, string username, string password, string gender, int age, int mobile, string address, int selectedPatientID)
+        {
+            string query = "update Patient set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'" +
+               "where PID = " + selectedPatientID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*-----------------------P_UpdateInfo-----------------*/
+        public int P_UpdateInfo(string name, string username, string password, string gender, int age, int mobile, string address, int loggedOnPatientID)
+        {
+            string query = "update Patient set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'" +
+               "where PID = " + loggedOnPatientID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*------------------D_UpdateInfo----------------------*/
+ 
+        public int D_UpdateInfo(string name, string username, string password, string gender, int age, int mobile, string address, int deptID, string startWH, string endWH, int salary, double avgRate, int loggedONDrID)
+        {
+            string query = "update Doctor set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'," +
+               "Department_ID=" + deptID + "," +
+               "Start_of_Working_Hours='" + startWH + "'," +
+               "End_of_Working_Hours='" + endWH + "'," +
+               "Salary=" + salary + "," +
+               "Avg_Rating=" + avgRate +
+               "where DID = " + loggedONDrID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*----------------R_UpdateInfo--------------------*/
+        public int R_UpdateInfo(string name, string username, string password, string gender, int age, int mobile, string address, int salary, int loggedOnRecepID)
+        {
+            string query = "update Receptionist set " +
+               "Name ='" + name + "'," +
+               "Username = '" + username + "'," +
+               "Password ='" + password + "'," +
+               "Gender='" + gender + "'," +
+               "Age=" + age + "," +
+               "Mobile_Number=" + mobile + "," +
+               "Address='" + address + "'," +
+               "Salary=" + salary +
+               "where RecID = " + loggedOnRecepID;
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*------------------A_UpdateInfo---------------------*/
+        public int A_UpdateInfo(string name, string username, string password, int loggedOnAdmin)
+        {
+            string query = "update Admin set " +
+               "Name='" + name + "'," +
+               "Username='" + username + "'," +
+               "Password='" + password + "'" +
+               "where AID = " + loggedOnAdmin + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+                                                            /*-----------------------------------------*/
+                                                            /*-------------------GROUP D---------------*/
+                                                            /*-----------------------------------------*/
+
+        /*-------------A_AddUpdateDepartment--------------*/
+
+        public int A_AddDepartment(string name)
+        {
+            string query = "INSERT INTO Department (Name) VALUES (" +
+               "'" + name + "'" +")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int A_UpdateDepartment(string name, int selectedDeptID)
+        {
+            string query = "update Department set " +
+               "Name='" + name + "'" +
+               "where DepID = " + selectedDeptID + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*----------------A_AddUpdateRoom----------------*/
+        
+        public int A_AddRoom(string type, int size, int price, int deptID, bool availability)
+        {
+            string query = "INSERT INTO Room (Type,Size,Price,Department_ID,Room_Availability) VALUES (" +
+               "'" + type + "'," +
+               "" + size + "," +
+               "" + price + "," +
+               "" + deptID + "," +
+               "" + availability + ")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int A_UpdateRoom(string type, int size, int price, int deptID, bool availability, int selectedRoomID )
+        {
+            string query = "update Room set " +
+               "Type='" + type + "'," +
+               "Size=" + size + "," +
+               "Price=" + price + "," +
+               "Department_ID=" + deptID + "," +
+               "Room_Availability=" + availability  +
+               "where RID = " + selectedRoomID + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*--------------A_AddUpdateMedication--------------*/
+        public int A_AddMedication(string name, int price, string exp_date)
+        {
+            string query = "INSERT INTO Medication (Name,Price,Exp_Date)  VALUES (" +
+               "'" + name + "'," +
+               "" + price + "," +
+               "'" + exp_date + "'" +")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int A_UpdateMedication(string name, int price, string exp_date, int selectedMedicationID)
+        {
+            string query = "update Medication set " +
+               "Name='" + name + "'," +
+               "Price=" + price + "," +
+               "Exp_Date = '" + exp_date + "'" +
+               "where RID = " + selectedMedicationID + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        /*-------------A_AddUpdateScan-----------*/
+       
+        public int A_AddScan(string name, int price)
+        {
+            string query = "INSERT INTO Scan (Name,Price)  VALUES (" +
+               "'" + name + "'," +
+               "" + price  + ")";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+        public int A_UpdateScan(string name, double price, int selectedMedicationID)
+        {
+            string query = "update Medication set " +
+               "Name='" + name + "'," +
+               "Price=" + price +
+               "where SID = " + selectedMedicationID + ";";
+
+            return dbMan.ExecuteNonQuery(query);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
